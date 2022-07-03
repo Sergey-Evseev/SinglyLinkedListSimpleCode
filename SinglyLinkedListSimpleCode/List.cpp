13c874a5742116cf5ecd7094efba88d8e1766a45
Sergey Evseev <sevseev@gmail.com>
2022-07-04 01:50:41 +0300

    first part

--- SinglyLinkedListSimpleCode/List.cpp
+++ SinglyLinkedListSimpleCode/List.cpp
@@ -1,3 +1,51 @@
 #include "List.h"
 #include <iostream>
 using namespace std;
+
+template<typename T>
+List<T>::List()
+{
+	Size = 0;//начальный размер списка
+	head = nullptr;//первоначально указывает на ноль
+};
+
+template<typename T>
+List<T>::~List() //деструктор класcа List
+{
+	cout << "Destructor" << endl;
+};
+
+template<typename T> 
+void List<T>:: push_back(T data)//метод добавления данных, имя метода по типу как в стандартной бибилиотеке 
+{
+	if (head == nullptr) //если указатель пустой
+	{
+		head = new Node<T>(data); //создать новый эл-т Node и передать ему в конструктор данные
+	}                             //head будет указывать на новую ноду в динамической памяти - объект класса Node 
+	else
+	{
+		Node <T> *current = this->head; //временная перем. типа Node, присваиваем ей значение заголовка 
+		
+		while (current->pNext != nullptr ) //в цикле идем по нодам и сравниваем на что указывает заголовок текущего элемента
+		{
+			current = current->pNext; //если заголовок на след.эл-т не указывает на нуль, врем. переменной (адресу) присваиваем значение следующего
+		}
+		current->pNext = new Node<T>(data);//переменной pNext последнего (с нул. адресом) элемента присваиваем значение новой ноды
+	}
+	Size++; //при каждом добавлении элемента увеличивать переменную Size
+}
+template<typename T>
+T & List<T>::operator[](const int index)
+{
+	int counter = 0; //счетчик в каком элементе мы находимся
+	Node<T> *current = this->head;//переменная для проверки следующего элемента
+	while (current != nullptr)
+	{
+		if (counter == index)//если счетчик совпадает с номер запрошенного элемента 
+		{ 
+			return current->data //то вернуть значение его поля data
+		}
+		current = current->pNext; //пока current не станет указывать на null присваивать текущему адрес следующего
+		counter++;
+	}
+}
--- SinglyLinkedListSimpleCode/SinglyLinkedListSimpleCode.cpp
+++ SinglyLinkedListSimpleCode/SinglyLinkedListSimpleCode.cpp
@@ -1,17 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include "List.h";
+#include "List.h"
 using namespace std;
 
 int main()
 {
+	setlocale(LC_ALL, "ru");
+	List<int> lst; //объект типа List,передаем тип данных в шаблон конструктора класса  
+	lst.push_back(5);
+	lst.push_back(10); //повторный вызов для создания второго элемента
+	lst.push_back(20); 
+
+	lst.GetSize();//у списка можем вызвать геттер и посмотреть кол-во элементов
+	cout << lst[2] << endl; //вывод значения элемента по его индексу
+
+	int numbersCount; //переменная размеров списка для заполнения случай. значениями
+	cin >> numbersCount;
+	for (int i = 0; i < numbersCount; i++)//заполнение списка на numbersCount 'ktvtynjd
+	{
+		lst.push_back(rand()%10);
+	}
 
 
+	for (int i = 0; i < lst.GetSize(); i++)//вывод всех элементов списка
+	{
+		cout << lst[i] << endl;
+	}
 
-
-
-
-
-
-
+	return 0;
 }
-
